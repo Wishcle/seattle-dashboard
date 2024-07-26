e3f27f4662c4e175a08750ea428d99aef90fffe5
Wishcle <cpmqaq@domain.com>
2024-07-26 14:05:26 -0400

    Move data downloader to command class; add stubs
    This commit creates class LoadDatabaseCmd in downloader.py. This class
    will have a single 'public' method, `exec`, which will download data
    from SDOT and create a table for it. Stub methods will be implemented in
    followup commits.

--- server/downloader.py
+++ server/downloader.py
@@ -1,12 +1,9 @@
 
 import requests
-import json
 import time
 from tqdm import tqdm
 from pathlib import Path
 from http import HTTPStatus
-import utm
-from pyproj import Transformer
 
 BASE_URL = "https://data-seattlecitygis.opendata.arcgis.com/api/download/v1/items/{item}/{filetype}?layers=0"
 URL_CSV_COLLISIONS = BASE_URL.format(item="504838adcb124cf4a434e33bf420c4ad", filetype="csv")
@@ -16,8 +13,31 @@ URL_CSV_VEHICLES   = BASE_URL.format(item="90a68d4709b54327a6bc1dfa1b900f8d", fi
 DATA_DIR = Path("data")
 
 def main():
-    download(URL_CSV_PERSONS, "persons.csv")
-    download(URL_CSV_COLLISIONS, "collisions.csv")
+    LoadDatabaseCmd(URL_CSV_PERSONS, "persons.csv").exec()
+    LoadDatabaseCmd(URL_CSV_COLLISIONS, "collisions.csv").exec()
+
+
+class LoadDatabaseCmd():
+    url: str  # The url to fetch the data from.
+    file_name: str  # Where to put the downloaded data.
+
+    def __init__(self, url: str, file_name: str) -> None:
+        self.url = url
+        self.file_name = file_name
+
+    def exec(self) -> None:
+        self._download_data_from_sdot()
+        self._load_data_into_database()
+        self._print_tables()
+
+    def _download_data_from_sdot(self) -> None:
+        download(self.url, self.file_name)
+
+    def _load_data_into_database(self) -> None:
+        pass
+
+    def _print_tables(self) -> None:
+        pass
 
 
 def download(url: str, file_name: Path) -> None:
