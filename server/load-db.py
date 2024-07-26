abe6544d2bff6c3d8190d916557dc9d6d656bb1d
Wishcle <cpmqaq@domain.com>
2024-07-26 14:36:54 -0400

    load-db: Move download functions into command class

--- server/load-db.py
+++ server/load-db.py
@@ -1,6 +1,7 @@
 
 import requests
 import time
+from typing import Optional
 from tqdm import tqdm
 from pathlib import Path
 from http import HTTPStatus
@@ -21,6 +22,8 @@ class LoadDatabaseCmd():
     url: str  # The url to fetch the data from.
     file_name: str  # Where to put the downloaded data.
 
+    response: Optional[requests.Response] = None
+
     def __init__(self, url: str, file_name: str) -> None:
         self.url = url
         self.file_name = file_name
@@ -31,50 +34,51 @@ class LoadDatabaseCmd():
         self._print_tables()
 
     def _download_data_from_sdot(self) -> None:
-        download(self.url, self.file_name)
+        self._poll_for_ok_response()
+        self._download_from_response()
+
+    def _poll_for_ok_response(self) -> requests.Response:
+        assert self.response is None
+        sleep_time = 15
+        while True:
+            print("making request ... ", end="", flush=True)
+            response = requests.get(self.url)
+            status = HTTPStatus(response.status_code)
+
+            if status == HTTPStatus.ACCEPTED:
+                server_status = response.json()["status"]
+                print(f"request ACCEPTED; server says: {server_status}; sleep {sleep_time}")
+                time.sleep(sleep_time)
+                sleep_time += 5
+                continue
+
+            if status == HTTPStatus.OK:
+                self.response = response
+                return
+
+            raise RuntimeError(f"Unexpected status: {status.value} {status.phrase}")
+
+    def _download_from_response(self):
+        assert self.response is not None
+        assert self.response.status_code == HTTPStatus.OK
+
+        print(f"request OK; downloading file to {self.file_name=}")
+        DATA_DIR.mkdir(exist_ok=True)
+        file_name = DATA_DIR / self.file_name
+        total_size = int(self.response.headers.get('content-length', 0))
+        progress_bar = tqdm(total=total_size, unit='B', unit_scale=True)
+
+        with tqdm(total=total_size, unit='B', unit_scale=True) as progress_bar:
+            with open(file_name, 'wb') as file:
+                for data in self.response.iter_content(chunk_size=1024):
+                    progress_bar.update(len(data))
+                    file.write(data)
 
     def _load_data_into_database(self) -> None:
         pass
 
     def _print_tables(self) -> None:
         pass
-
-
-def download(url: str, file_name: Path) -> None:
-    sleep_time = 15
-    while True:
-        print("making request ... ", end="", flush=True)
-        response = requests.get(url)
-        status = HTTPStatus(response.status_code)
-
-        if status == HTTPStatus.ACCEPTED:
-            server_status = response.json()["status"]
-            print(f"request ACCEPTED; server says: {server_status}; sleep {sleep_time}")
-            time.sleep(sleep_time)
-            sleep_time += 5
-            continue
-
-        if status == HTTPStatus.OK:
-            print(f"request OK; downloading file to {file_name=}")
-            download_from_response(response, file_name)
-            return
-
-        raise RuntimeError(f"Unexpected status: {status.value} {status.phrase}")
-
-
-def download_from_response(response: requests.Response, file_name: Path):
-    DATA_DIR.mkdir(exist_ok=True)
-    file_name = DATA_DIR / file_name
-
-    assert response.status_code == HTTPStatus.OK
-    total_size = int(response.headers.get('content-length', 0))
-    progress_bar = tqdm(total=total_size, unit='B', unit_scale=True)
-
-    with tqdm(total=total_size, unit='B', unit_scale=True) as progress_bar:
-        with open(file_name, 'wb') as file:
-            for data in response.iter_content(chunk_size=1024):
-                progress_bar.update(len(data))
-                file.write(data)
 
 
 if __name__ == "__main__":
