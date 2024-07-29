14230b3960d0142673a632bf6d4104af4c09bd8b
Wishcle <cpmqaq@domain.com>
2024-07-28 23:30:36 -0400

    load-db: Implement the db-loading stuff
    This commit implements _load_data_into_database. Now after running the
    script, we have a local sqllite database with 'collisions' and 'persons'
    tables.
    
    Also, the downloading functions now use the sched library's scheduler to
    poll for both tables' data at the same time. Maybe a bit over-
    complicated?
    
    I've been forgetting to update requirements.txt. This commit includes
    all the libraries installed so far.

--- server/load-db.py
+++ server/load-db.py
@@ -1,36 +1,47 @@
 
+import sched
 import sqlite3
 import time
+from dataclasses import dataclass
 from http import HTTPStatus
 from pathlib import Path
-from typing import Optional
+from typing import List, Optional
 
+import pandas
 import requests
 from tqdm import tqdm
+
+DATABASE_ROOT = Path("db")
+DATABASE_FILE = DATABASE_ROOT / Path("seattle.db")
+DATABASE_CACHE = DATABASE_ROOT / Path("cache")
 
 BASE_URL = "https://data-seattlecitygis.opendata.arcgis.com/api/download/v1/items/{item}/{filetype}?layers=0"
 URL_CSV_COLLISIONS = BASE_URL.format(item="504838adcb124cf4a434e33bf420c4ad", filetype="csv")
 URL_CSV_PERSONS    = BASE_URL.format(item="f3e9dd827e934649972cd7469474598a", filetype="csv")
 URL_CSV_VEHICLES   = BASE_URL.format(item="90a68d4709b54327a6bc1dfa1b900f8d", filetype="csv")
 
-DATABASE_ROOT = Path("db")
-DATABASE_FILE = DATABASE_ROOT / Path("seattle.db")
-DATABASE_CACHE = DATABASE_ROOT / Path("cache")
+@dataclass
+class Table():
+    name: str
+    url: str  # To fetch the data from.
+    url_fetch_delay: int = 0
+    url_response: Optional[requests.Response] = None
+    file_path: Optional[Path] = None
+
 
 def main():
-    LoadDatabaseCmd(URL_CSV_PERSONS, "persons.csv").exec()
-    LoadDatabaseCmd(URL_CSV_COLLISIONS, "collisions.csv").exec()
+    tables = [
+        Table("collisions", URL_CSV_COLLISIONS),
+        Table("persons", URL_CSV_PERSONS)]
+    LoadDatabaseCmd(tables).exec()
 
 
 class LoadDatabaseCmd():
-    url: str  # The url to fetch the data from.
-    file_name: str  # Where to put the downloaded data.
+    tables: List[Table]
+    scheduler: Optional[sched.scheduler] = None
 
-    response: Optional[requests.Response] = None
-
-    def __init__(self, url: str, file_name: str) -> None:
-        self.url = url
-        self.file_name = file_name
+    def __init__(self, tables: List[Table]) -> None:
+        self.tables = tables
 
     def exec(self) -> None:
         self._init_folders()
@@ -44,51 +55,63 @@ class LoadDatabaseCmd():
         DATABASE_FILE.unlink(missing_ok=True)
 
     def _download_data_from_sdot(self) -> None:
-        self._poll_for_ok_response()
-        self._download_from_response()
+        print("Downloading data...")
+        self.scheduler = sched.scheduler()
+        for table in self.tables:
+            self.scheduler.enter(0, 0, self._poll_for_ok_response, [table])
+        self.scheduler.run()
+        print("...finished.\n")
 
-    def _poll_for_ok_response(self) -> requests.Response:
-        assert self.response is None
-        sleep_time = 15
-        while True:
-            print("making request ... ", end="", flush=True)
-            response = requests.get(self.url)
-            status = HTTPStatus(response.status_code)
+    def _poll_for_ok_response(self, table: Table) -> None:
+        print(f"[{table.name}] making request ... ", flush=True)
+        response = requests.get(table.url)
+        status = HTTPStatus(response.status_code)
 
-            if status == HTTPStatus.ACCEPTED:
-                server_status = response.json()["status"]
-                print(f"request ACCEPTED; server says: {server_status}; sleep {sleep_time}")
-                time.sleep(sleep_time)
-                sleep_time += 5
-                continue
+        # If ACCEPTED, reschedule the poll with linear back-off.
+        # We can't download anything until the site says OK.
+        if status == HTTPStatus.ACCEPTED:
+            table.url_fetch_delay += 3
+            server_status = response.json()["status"]
+            print(f"[{table.name}] ... request ACCEPTED; server says: {server_status}; sleep {table.url_fetch_delay}", flush=True)
+            self.scheduler.enter(table.url_fetch_delay, 0, self._poll_for_ok_response, [table])
+            return
 
-            if status == HTTPStatus.OK:
-                self.response = response
-                return
+        if status == HTTPStatus.OK:
+            table.url_response = response
+            self._download_from_response(table)
+            return
 
-            raise RuntimeError(f"Unexpected status: {status.value} {status.phrase}")
+        raise RuntimeError(f"Unexpected status: {status.value} {status.phrase}")
 
-    def _download_from_response(self):
-        assert self.response is not None
-        assert self.response.status_code == HTTPStatus.OK
+    def _download_from_response(self, table: Table) -> None:
+        assert table.url_response is not None
         assert DATABASE_CACHE.exists()
 
-        print(f"request OK; downloading file to {self.file_name=}")
-        file_name = DATABASE_CACHE / self.file_name
-        total_size = int(self.response.headers.get('content-length', 0))
-        progress_bar = tqdm(total=total_size, unit='B', unit_scale=True)
+        table.file_path = DATABASE_CACHE / f"{table.name}.csv"
+        total_size = int(table.url_response.headers.get('content-length', 0))
+        progress_bar = tqdm(total=total_size, unit='B', unit_scale=True, leave=False)
 
+        print(f"[{table.name}] ... request OK; downloading file to {table.file_path=}", flush=True)
         with tqdm(total=total_size, unit='B', unit_scale=True) as progress_bar:
-            with open(file_name, 'wb') as file:
-                for data in self.response.iter_content(chunk_size=1024):
+            with open(table.file_path, 'wb') as file:
+                for data in table.url_response.iter_content(chunk_size=1024):
                     progress_bar.update(len(data))
                     file.write(data)
 
     def _load_data_into_database(self) -> None:
-        pass
+        with sqlite3.connect(DATABASE_FILE) as conn:
+            for table in self.tables:
+                with open(table.file_path) as csv_file:
+                    df = pandas.read_csv(csv_file)
+                    df.to_sql(table.name, conn, if_exists='append', index=False)
+                    print(f"table '{table.name}' created.")
 
     def _print_tables(self) -> None:
-        pass
+        print(f"Here are the tables in {DATABASE_FILE}:")
+        with sqlite3.connect(DATABASE_FILE) as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+            print(cursor.fetchall())
 
 
 if __name__ == "__main__":
