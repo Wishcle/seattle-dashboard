17bc3c6e8f3dd5465327d5f2372b343b1a526d9f
Wishcle <cpmqaq@gmail.com>
2024-09-04 12:21:10 -0400

    map: Add heatmap renderer for collisions data
    This commit changes the way collision data is displayed on the map.
    Before, we had an orange dot for each collision; now, after three
    seconds, the orange dots turn into a heatmap. (This was just done to
    showcase both rendering strategies at once and also to commit an
    async-await-timeout example).
    
    Currently, the heatmap treats all collisions equally. We'll need to
    figure out how to have the heatmap take into account particular metrics
    of a collision to give some more "heat" than others. Also, there's
    probably some backend work with the 'collision' and 'person' tables to
    get those metrics (i.e. which injured persons were on a bike).
    
    About heatmap color scale: https://tinyurl.com/sdb-veridis

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -6,9 +6,19 @@ import Graphic from "@arcgis/core/Graphic";
 import Point from "@arcgis/core/geometry/Point";
 import SimpleMarkerSymbol from "@arcgis/core/symbols/SimpleMarkerSymbol.js";
 import SimpleLineSymbol from "@arcgis/core/symbols/SimpleLineSymbol.js";
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer.js";
+import HeatmapRenderer from "@arcgis/core/renderers/HeatmapRenderer.js";
+import HeatmapColorStop from "@arcgis/core/renderers/support/HeatmapColorStop.js";
+import SimpleRenderer from "@arcgis/core/renderers/SimpleRenderer.js";
+import Gradient from "javascript-color-gradient";
 
 const MAP_DIV_ID = "mapDiv"
 const URL_COLLISIONS_MANY = "http://127.0.0.1:5000/collisions/many"
+
+
+// ============= //
+//   INTERFACES  //
+// ============= //
 
 interface Props {
 }
@@ -32,6 +42,51 @@ interface Feature {
 }
 
 
+// ============ //
+//   RENDERERS  //
+// ============ //
+
+// Render data as a heatmap.
+const RENDERER_HEATMAP = (() => {
+  // https://cran.r-project.org/web/packages/viridis/vignettes/intro-to-viridis.html
+  const veridisColors = ["#440154", "#3b528b", "#21918c", "#5ec962", "#fde725"];
+  const gradientColors = new Gradient()
+    .setColorGradient(...veridisColors)
+    .setMidpoint(12)
+    .getColors();
+
+  const heatmapColors = ["rgba(0, 0, 0, 0)"].concat(gradientColors)
+  const heatmapColorStops = heatmapColors.map(
+    (c, i) => new HeatmapColorStop({
+      color: c, ratio: i / (heatmapColors.length - 1)
+    }
+  ));
+
+  return new HeatmapRenderer({
+    colorStops: heatmapColorStops,
+    radius: 10,
+    maxDensity: 0.04625,
+    minDensity: 0,
+  });
+})();
+
+
+// Render points as orange dots.
+const RENDERER_POINTS = (() => {
+  const markerSymbol = new SimpleMarkerSymbol({
+    color: [226, 119, 40],
+    outline: new SimpleLineSymbol({
+      color: [255, 255, 255],
+      width: 2,
+    }),
+  });
+
+  return new SimpleRenderer({
+    symbol: markerSymbol,
+  });
+})();
+
+
 // ============= //
 //   COMPONENT   //
 // ============= //
@@ -42,7 +97,7 @@ const MapComp:FC<Props> = (_props) => {
 
   const createMapView = () => {
     const map = new Map({
-      basemap: "streets",
+      basemap: "dark-gray",
     });
 
     const extent = {
@@ -65,31 +120,36 @@ const MapComp:FC<Props> = (_props) => {
   }
 
   const plotFeatures = () => {
-    if (mapView == null) {
-      return;  // Do nothing if the map view does not exist yet.
+    if (mapView == null || features.length == 0) {
+      return;
     }
-
-    const markerSymbol = new SimpleMarkerSymbol({
-      color: [226, 119, 40],
-      outline: new SimpleLineSymbol({
-        color: [255, 255, 255],
-        width: 2,
-      }),
-    });
 
     const featureToGraphic = (feature: Feature) => {
       const [x, y] = feature.geometry.coordinates;
       const spatialReference = { wkid: 2926 };
       const point = new Point({ x, y, spatialReference });
-      return new Graphic({
-        geometry: point,
-        symbol: markerSymbol,
-      });
+      return new Graphic({ geometry: point });
     };
 
-    const graphics = features.map(featureToGraphic)
-    mapView.graphics.removeAll()
-    mapView.graphics.addMany(graphics)
+    const graphics = features.map(featureToGraphic);
+
+    const layer = new FeatureLayer({
+      source: graphics,
+      objectIdField: "bogus",  // Required, but what I put here does not change how points are drawn.
+      geometryType: "point",
+      renderer: RENDERER_POINTS,
+    });
+
+    mapView.map.add(layer);
+
+    // For now, just to showcase both renderers, switch to the heatmap after three seconds.
+    (async () => {
+      const timeout = (delay: number) =>
+        new Promise(resolve => setTimeout(resolve, delay));
+      await timeout(3000);
+      console.log("point renderer");
+      layer.renderer = RENDERER_HEATMAP;
+    })();
   };
 
   const fetchCollisions = () => {
